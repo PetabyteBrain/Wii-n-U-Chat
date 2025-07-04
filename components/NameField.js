3930efc9b2d0e7cb4b689f90d8f11f1f92b54645
Spyros Catechis <bcates@bbcag.ch>
2025-07-04 11:05:48 +0200

    added Keyboard avoiding view

--- components/NameField.js
+++ components/NameField.js
@@ -1,33 +1,55 @@
 import React from "react";
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import SendArrow from "../assets/send-arrow.svg";
 
 export default function NameField({ username, setUsername, onSubmit }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Enter a Username to Start</Text>
-      <TextInput
-        placeholder="Username..."
-        placeholderTextColor="#aaa"
-        value={username}
-        onChangeText={setUsername}
-        style={styles.input}
-      />
-      
-      <TouchableOpacity onPress={onSubmit} activeOpacity={0.8} style={{ borderRadius: 15, overflow: "hidden" }}>
-        <LinearGradient
-          colors={['rgba(66, 109, 130, 0.29)', 'rgba(66, 109, 130, 0.29)']}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 0, y: 1 }}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Submit</Text>
-          <SendArrow width={20} height={20} style={{ marginLeft: 6 }} />
-        </LinearGradient>
-      </TouchableOpacity>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 120 : 80}
+        style={{flex: 1, justifyContent: "center", alignItems: "center"}}
+      >
+        <View style={styles.container}>
+          <Text style={styles.label}>Enter a Username to Start</Text>
+          <TextInput
+            placeholder="Username..."
+            placeholderTextColor="#aaa"
+            value={username}
+            onChangeText={setUsername}
+            style={styles.input}
+          />
+
+          <TouchableOpacity
+            onPress={onSubmit}
+            activeOpacity={0.8}
+            style={{ borderRadius: 15, overflow: "hidden" }}
+          >
+            <LinearGradient
+              colors={["rgba(66, 109, 130, 0.29)", "rgba(66, 109, 130, 0.29)"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 0, y: 1 }}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Submit</Text>
+              <SendArrow width={20} height={20} style={{ marginLeft: 6 }} />
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -44,6 +66,7 @@ const styles = StyleSheet.create({
     marginBottom: 6,
   },
   input: {
+    minWidth: 120,
     width: "100%",
     paddingVertical: 8,
     paddingHorizontal: 12,
